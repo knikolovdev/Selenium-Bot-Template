82d016d4525a29a759e4699b594e5119647102a5
Konstantin <Konstantin@FRABATO-LAPTOP>
2016-04-20 05:02:19 +0300

    small changes

--- Selenium bot template/Properties/Settings.Designer.cs
+++ Selenium bot template/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace Selenium_bot_template.Properties {
                 this["proxy"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Chrome")]
+        public string browser {
+            get {
+                return ((string)(this["browser"]));
+            }
+            set {
+                this["browser"] = value;
+            }
+        }
     }
 }
